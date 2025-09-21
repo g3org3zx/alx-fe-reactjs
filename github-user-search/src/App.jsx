a28d0b329cfe4d072ecd81e1b7b07d10bb897a50
George Kamande <161124335+g3org3zx@users.noreply.github.com>
2025-09-21 19:02:51 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,17 @@
-import React from "react";
-import "./style.css"; // Import stylesheet
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Search from './components/Search';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>GitHub User Search</h1>
-      <p>Welcome to Task 0 setup.</p>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>GitHub User Search</h1>
+        <Routes>
+          <Route path="/" element={<Search />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
