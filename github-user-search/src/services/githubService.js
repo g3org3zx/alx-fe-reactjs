fddc25343aef9b8957c1dd28f4048b5e78f2d149
George Kamande <161124335+g3org3zx@users.noreply.github.com>
2025-09-21 20:00:27 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-const GITHUB_API_URL = 'https://api.github.com/users';
+   const GITHUB_API_URL = import.meta.env.VITE_APP_API_BASE_URL || 'https://api.github.com';
+   const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
-export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`${GITHUB_API_URL}/${username}`);
-    return response.data;
-  } catch (error) {
-    throw new Error('Looks like we cant find the user');
-  }
-};
+   export const fetchUserData = async (username) => {
+     try {
+       const headers = API_KEY ? { Authorization: `token ${API_KEY}` } : {};
+       const response = await axios.get(`${GITHUB_API_URL}/users/${username}`, { headers });
+       return response.data;
+     } catch (error) {
+       throw new Error('Looks like we cant find the user');
+     }
+   };
